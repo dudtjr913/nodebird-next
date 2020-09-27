e81f910d29a53bc0928b0177581def93d6cd1c5b
unknown <dudtjr913@naver.com>
2020-09-27 21:02:15 +0900

    scrollToY

--- front/components/ScreenUp.js
+++ front/components/ScreenUp.js
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { UpSquareOutlined } from '@ant-design/icons';
-import { element } from 'prop-types';
 
-const ScreenUp = () => (
-  <div
-    style={{
-      position: 'fixed',
-      right: '30px',
-      bottom: '30px',
-      fontSize: '50px',
-    }}
-  >
-    <UpSquareOutlined />
-  </div>
-);
+const ScreenUp = () => {
+  const handleOnScreenUp = useCallback(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }, []);
+  return (
+    <div
+      onClick={handleOnScreenUp}
+      role="presentation"
+      style={{
+        position: 'fixed',
+        right: '30px',
+        bottom: '30px',
+        fontSize: '50px',
+        zIndex: '2000',
+        cursor: 'pointer',
+      }}
+    >
+      <UpSquareOutlined />
+    </div>
+  );
+};
 
 export default ScreenUp;
